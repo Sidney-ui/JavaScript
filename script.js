a5c35a2c1fbfdfbe6ae69a5fea1273855b5576ae
SIDNEY ARTHUR AZEVEDO <sidney_azevedo@estudante.sesisenai.org.br>
2025-04-01 14:01:52 -0300

    coisas adicionadas

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+var nome = "maria"
+let valor = 2.3
+let idade = 15
+var casado = false
+let pessoa = {nome: "Lucia", idade: 12}
+let cores = ["amarelo", "vermelho", "verde"]
+
+dodocument.weitein(nome+ "<br>")// adiciona uma quebra de linhas após cada valor
+
+document.writeln(valor+"<br>")
+document.writeln(idade+"<br>")
+document.writeln(casado+"<br>")
+document.writeln(JSON.stringify(pessoa) + "<br>")// para exibir o objeto de forma legivel
+document.writeln(cores+ "<br>") //para exibir array de forma legivel
+
